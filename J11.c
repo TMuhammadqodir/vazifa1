ccae14fe3f61abaeb6e1461750b18b00bf1a2b9e
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2022-11-25 18:52:49 +0500

    Add files

--- J11.c
+++ J11.c
@@ -7,7 +7,7 @@ void input(int array[]){
     }
 }
 void second_min(int array[]){
-    int temp;
+    int temp,temp1;
     for(int i=0; i<10; i++){
         for(int j=i+1; j<10; j++){
             if(array[i]>array[j]){
@@ -17,7 +17,13 @@ void second_min(int array[]){
             }
         }
     }
-    printf("%d",array[1]);
+    temp1=array[0];
+    for(int i=0; i<10; i++){
+        if(temp1<array[i]){
+            printf("%d ",array[i]);
+            break;
+        }
+    }
 }
 
             // j11-misol
