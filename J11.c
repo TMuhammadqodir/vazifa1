2ee6b05b637d98932b2ba8a2eea2eb7cdd53392d
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2022-11-25 15:31:24 +0500

    Add files

---
+++ J11.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+void input(int array[]){
+    for(int i=0; i<10; i++){
+        scanf("%d",&array[i]);
+    }
+}
+void second_min(int array[]){
+    int temp;
+    for(int i=0; i<10; i++){
+        for(int j=i+1; j<10; j++){
+            if(array[i]>array[j]){
+                temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
+        }
+    }
+    printf("%d",array[1]);
+}
+
+            // j11-misol
+int main(){
+    int a[10];
+    input(a);
+    second_min(a);
+   
+    return 0;
+}
